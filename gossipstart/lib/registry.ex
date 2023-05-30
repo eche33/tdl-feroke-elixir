766044562ecd7384b65932294fba9f41cbf13956
Rodrigo Etchegaray <rorroeche@gmail.com>
2023-05-30 19:14:29 -0300

    Cleanup in registry module

--- gossipstart/lib/registry.ex
+++ gossipstart/lib/registry.ex
@@ -8,18 +8,6 @@ defmodule Gossipstart.Registry do
     GenServer.start_link(__MODULE__, :ok, opts)
   end
 
-  @doc """
-  Looks up the bucket pid for `name` stored in `server`.
-
-  Returns `{:ok, pid}` if the bucket exists, `:error` otherwise.
-  """
-  def lookup(server, name) do
-    GenServer.call(server, {:lookup, name})
-  end
-
-  @doc """
-  Ensures there is a bucket associated with the given `name` in `server`.
-  """
   def create(server, rumor) do
     GenServer.call(server, {:create, rumor})
   end
@@ -32,10 +20,6 @@ defmodule Gossipstart.Registry do
     GenServer.cast(server, {:create_all_nodes})
   end
 
-  def received_rumor() do
-    GenServer.call(@me, {:received_rumor})
-  end
-
   ## Defining GenServer Callbacks
 
   @impl true
@@ -44,39 +28,8 @@ defmodule Gossipstart.Registry do
   end
 
   @impl true
-  def handle_call({:lookup, name}, _from, names) do
-    {:reply, Map.fetch(names, name), names}
-  end
-
-  # @impl true
-  # def handle_cast({:create, name}, {names, refs}) do
-  #   if Map.has_key?(names, name) do
-  #     {:noreply, {names, refs}}
-  #   else
-  #     {:ok, pid} = DynamicSupervisor.start_child(KV.BucketSupervisor, KV.Bucket)
-  #     ref = Process.monitor(pid)
-  #     refs = Map.put(refs, ref, name)
-  #     names = Map.put(names, name, pid)
-  #     {:noreply, {names, refs}}
-  #   end
-  # end
-
-  @impl true
   def handle_cast({:create_node, name}, state) do
-    {:ok, pid} = DynamicSupervisor.start_child(Gossipstart.NodeSupervisor, {Gossipstart.Node, name})
-    {:noreply, state}
-  end
-
-  @impl true
-  def handle_cast({:create_all_nodes}, state) do
-    {:ok, pid} = DynamicSupervisor.start_child(Gossipstart.NodeSupervisor, {Gossipstart.Node, [:Node1, 2]})
-    {:ok, pid} = DynamicSupervisor.start_child(Gossipstart.NodeSupervisor, {Gossipstart.Node, [:Node2, 1]})
-    # {:ok, pid} = DynamicSupervisor.start_child(Gossipstart.NodeSupervisor, {Gossipstart.Node, [:Node3, 4]})
-    # {:ok, pid} = DynamicSupervisor.start_child(Gossipstart.NodeSupervisor, {Gossipstart.Node, [:Node4, 1]})
-
-    total_nodes = 2
-    GenServer.cast(:Node1, {:rumor, "rumor", total_nodes - 1})
-
+    {:ok, _pid} = DynamicSupervisor.start_child(Gossipstart.NodeSupervisor, {Gossipstart.Node, name})
     {:noreply, state}
   end
 
@@ -95,21 +48,5 @@ defmodule Gossipstart.Registry do
     GenServer.call(:Node1, {:rumor, rumor, total_nodes - 1})
     {:reply, state}
   end
-
-  @impl true
-  def handle_call({:rumor_received}, from, count) do
-    IO.puts("Node #{inspect from} received rumor")
-
-    count = count - 1
-    if count == 0 do
-      IO.puts("Todos tienen el rumor")
-      System.halt(0)
-    else
-      {:reply, count, count}
-    end
-
-  end
-
-
 
 end
