ff3c38af9aad13e53ae2a3bafa80330a51e95f51
Rodrigo Etchegaray <rorroeche@gmail.com>
2023-06-25 12:16:17 -0300

    Timer parameters as constants

--- epidemic_simulator/lib/epidemic_simulator/medical_center.ex
+++ epidemic_simulator/lib/epidemic_simulator/medical_center.ex
@@ -2,13 +2,14 @@ defmodule EpidemicSimulator.MedicalCenter do
   use GenServer
 
   @me __MODULE__
+  @timer_name :MedicalCenter_timer
+  @timer_period 1
 
   def start_link(opts) do
     GenServer.start_link(@me, :ok, opts)
   end
 
   def plot(citizens, simulation_step) do
-    IO.puts("PLOTEO")
     census_data =
       citizens
       |> Enum.map(fn {key, {x, y, health_status}} -> {x, y, key, "#{health_status}"} end)
@@ -22,12 +23,6 @@ defmodule EpidemicSimulator.MedicalCenter do
     next_step = state.step + 1
 
     %{state | step: next_step}
-  end
-
-  defp start_medical_center_timer() do
-    timer_name = String.to_atom("MedicalCenter_timer")
-    timer_period = 1
-    EpidemicSimulator.Timer.start_timer(timer_name, MedicalCenter, timer_period)
   end
 
   @impl true
@@ -61,32 +56,13 @@ defmodule EpidemicSimulator.MedicalCenter do
 
   @impl true
   def handle_cast(:ring, state) do
-    IO.puts("RING")
-    ## Le pusimos plot porque por ahora no lo llamaba un timer y entonces no tenía sentido el ring
-    ## Básicamente habría que llamar a este método la primera vez desde epidemic simulator (ya se hace)
+    Agent.stop(@timer_name)
 
-    # Con esto llamás al timer y le decís el tiempo. Habría que tener algo en el state que marque el final para no plotear infinitamente
-    # Ese estado lo podría cambiar el epidemic simulator cuando se terminó la simulación
-    # time = 1
-    # timer_identifier = String.to_atom("#{@me}_timer")
-    # EpidemicSimulator.Timer.start_timer(timer_identifier, @me, sick_time)
-
-    #timer_name = "MedicalCenter_timer"
-    #timer_name = String.to_atom(timer_name)
-    timer_name = String.to_atom("MedicalCenter_timer")
-    timer_period = 1
-
-    # stop timer
-    Agent.stop(timer_name)
-
-    plot(state.citizens, state.step)
+    new_state = plot_and_increment_step(state)
 
     if state.simulation_running do
-      EpidemicSimulator.Timer.start_timer(timer_name, MedicalCenter, timer_period)
+      EpidemicSimulator.Timer.start_timer(@timer_name, MedicalCenter, @timer_period)
     end
-
-    next_step = state.step + 1
-    new_state = %{state | step: next_step}
 
     {:noreply, new_state}
   end
@@ -95,7 +71,7 @@ defmodule EpidemicSimulator.MedicalCenter do
   def handle_cast(:start_census, state) do
     new_state = plot_and_increment_step(state)
 
-    start_medical_center_timer()
+    EpidemicSimulator.Timer.start_timer(@timer_name, MedicalCenter, @timer_period)
 
     {:noreply, new_state}
   end
